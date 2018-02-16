a1333a158302f86008c25a34861f1d5c6c0f1ec2
empoleonrocks <32292419+empoleonrocks@users.noreply.github.com>
2018-02-16 16:06:17 +0530

    Update plot1.py

--- plot1.py
+++ plot1.py
@@ -5,17 +5,13 @@ x=np.arange(0,3,1/8000.0)
 
 sine_wave=2048*np.sin(2*x*np.pi)
 cos_wave=2048*np.cos(2*x*np.pi)
-exponent_wave=2048*np.exp(x)
+exponent_wave= np.exp(x)
 
 plt.plot(x,np.sine_wave)
 plt.show()
 
+#plt.plot(x,np.cos_wave)
+#plt.show()
 
-
-plt.plot(x,np.cos_wave)
-plt.show()
-
-
-
-plt.plot(x,np.exponent_wave)
-plt.show()
+#plt.plot(x,np.exponent_wave)
+#plt.show()
